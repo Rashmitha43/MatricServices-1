189693481d543111185077721343b94eefc39c6e
Praneesha <pranesha182004@gmail.com>
2024-11-04 15:46:57 +0530

    debugging navbar issue

--- src/Layouts/MainLayout/Mainlayout.jsx
+++ src/Layouts/MainLayout/Mainlayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar';
+// import Navbar from '../../Components/Navbar/Navbar';
 import Footer from "../../Components/Footer/Footer"
 import { Outlet } from 'react-router-dom'
 const Mainlayout = () => {
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Outlet/>
     <Footer/>
     </>
